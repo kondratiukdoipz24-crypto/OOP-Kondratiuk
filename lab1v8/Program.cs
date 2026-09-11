2f15a6155a6cc45a05393be9b1c04f7324a5919f
DandelioN1728 <darynauyae876@gmail.com>
2026-09-11 17:19:20 +0300

    Lab1: додано клас Animal

---
+++ lab1v8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Animal
+{
+    private string species;
+    private string nickname;
+
+    public int Age { get; set; }
+
+    public Animal(string species, string nickname, int age)
+    {
+        this.species = species;
+        this.nickname = nickname;
+        Age = age;
+    }
+
+    public void Speak()
+    {
+        Console.WriteLine($"{nickname} ({species}), вік: {Age} років");
+        Console.WriteLine("Тварина видає звук.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal animal1 = new Animal("Собака", "Бобік", 3);
+        Animal animal2 = new Animal("Кіт", "Мурчик", 5);
+        Animal animal3 = new Animal("Папуга", "Кеша", 2);
+
+        animal1.Speak();
+        animal2.Speak();
+        animal3.Speak();
+    }
+}
